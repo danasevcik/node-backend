d9092de5046b83d5561b840b5ebe24d283564dc0
Dana Sevcik <danasevcik@hotmail.com>
2019-12-16 11:49:59 -0500

    change name of delete route

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.get('/', (req, res) => res.send('hello world'))
 app.get('/getAllUsers', (req, res) => main.getTableData(req, res, db))
 app.post('/createNewUser', (req, res) => main.postTableData(req, res, db))
 app.put('/editUser', (req, res) => main.putTableData(req, res, db))
-app.delete('/crud', (req, res) => main.deleteTableData(req, res, db))
+app.delete('/deleteUser', (req, res) => main.deleteTableData(req, res, db))
 
 // App Server Connection
 app.listen(process.env.PORT || 3000, () => {
